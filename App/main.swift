25fb95e38ab0340ec57b19c5713d72bd25836630
Ryan Collins <ryan@ryanmcollins.com>
2016-07-12 16:29:44 -0400

    Attempted upgrade to Vapor 0.13

--- App/main.swift
+++ App/main.swift
@@ -6,32 +6,41 @@ import VaporMustache
     Includes are relative to the Views (`Resources/Views`)
     directory by default.
 */
-let app = Application(providers: [
-    VaporMustache.Provider(withIncludes: [
+let mustache = VaporMustache.Provider(withIncludes: [
         "header": "Includes/header.mustache",
         "footer": "Includes/footer.mustache"
     ])
 ])
 
-app.get("/") { request in
+let workDir: String?
+#if Xcode
+    let parent = #file.characters.split(separator: "/").map(String.init).dropLast().joined(separator: "/")
+    workDir = "/\(parent)/.."
+#else
+    workDir = nil
+#endif
+
+let drop = Droplet(workDir: workDir, providers: [mustache])
+
+drop.get("/") { request in
     return try app.view("index.mustache", context: [
         "greeting": "Hello, world!"
     ])
 }
 
-app.get("blog") { request in
+drop.get("blog") { request in
     return try app.view("index.mustache", context: [
         "greeting": "Hello, world!"
     ])
 }
 
-app.get("admin/post") { request in
+drop.get("admin/post") { request in
     return try app.view("index.mustache", context: [
         "greeting": "Hello, world!"
     ])
 }
 
-let port = app.config["app", "port"].int ?? 80
+let port = drop.config["app", "port"].int ?? 80
 
 // Print what link to visit for default port
 print("Visit http://localhost:\(port)")
--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 11),
-		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/qutheory/vapor-sqlite.git", versions: Version(0,0,0)..<Version(10,0,0)),
+		.Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 9),
     ],
     exclude: [
 	    "Config",
