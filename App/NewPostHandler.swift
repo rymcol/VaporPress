4016e7ba2aabb6028d09e88db9cfe85d6b5a715a
Ryan Collins <ryan@ryanmcollins.com>
2016-07-21 18:10:57 -0400

    Added NewPostHandler

---
+++ App/NewPostHandler.swift
@@ -0,0 +1,37 @@
+import Vapor
+import VaporSQLite
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+
+struct IndexHandler {
+
+    func gatherContent() -> [String: Any] {
+
+        var finalDict = [String: Any]()
+
+        do {
+
+            let sqlite = try! VaporSQLite.Provider(path: "./resources/db/data.sqlite")
+            let results = try sqlite.driver.raw("SELECT post_content, post_title FROM posts WHERE id=(SELECT value FROM options WHERE option = 'front_page')")
+
+            for (key, value) in results[0] {
+                finalDict[key] = value
+            }
+
+        } catch {
+            print("sqlite Erros Occured")
+        }
+
+        finalDict["title"] = "Site Homepage"
+
+        let imageNumber = Int(arc4random_uniform(25) + 1)
+        finalDict["featuredImageURI"] = "/images/random/random-\(imageNumber).jpg"
+        
+        return finalDict
+    }
+
+}
