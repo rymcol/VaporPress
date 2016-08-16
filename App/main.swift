abd6f475a1849bd54cb419ac861501ca56a4652f
Ryan Collins <ryan@ryanmcollins.com>
2016-08-16 11:32:41 -0400

    Cleaned up Extraneous Code
    In Vapor 16, directory is no longer required, so that was removed, along with some old commented out code that was no longer necessary

--- App/main.swift
+++ App/main.swift
@@ -1,23 +1,12 @@
 import Vapor
 import VaporMustache
-/*
-    Includes are relative to the Views (`Resources/Views`)
-    directory by default.
-*/
+
 let mustache = VaporMustache.Provider(withIncludes: [
         "header": "Includes/header.mustache",
         "footer": "Includes/footer.mustache"
     ])
 
-let workDir: String?
-#if Xcode
-    let parent = #file.characters.split(separator: "/").map(String.init).dropLast().joined(separator: "/")
-    workDir = "/\(parent)/.."
-#else
-    workDir = nil
-#endif
-
-let drop = Droplet(workDir: workDir, initializedProviders: [mustache])
+let drop = Droplet(initializedProviders: [mustache])
 
 drop.get("/") { request in
     return try drop.view("index.mustache", context: IndexHandler().gatherContent())
