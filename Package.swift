0308b9901ff2237c8b0a4f7829683fa491db6daf
Ryan Collins <ryan@ryanmcollins.com>
2016-08-17 16:04:48 -0400

    Added Slick Fonts & Loaders

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ let package = Package(
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
-		.Package(url: "https://github.com/vapor/mustache-provider.git", majorVersion: 0, minor: 0),
     ],
     exclude: [
 	    "Config",
