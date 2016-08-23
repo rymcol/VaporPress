1e7f457cd33d321de8a15b873b79be8cd21568b7
Ryan Collins <ryan@ryanmcollins.com>
2016-08-23 16:01:47 -0400

    Added JSON
    Added a /json route and some random number generation for benchmarking JSON

--- App/main.swift
+++ App/main.swift
@@ -1,6 +1,12 @@
 import Vapor
 import HTTP
 
+#if os(Linux)
+public func arc4random_uniform(_ max: UInt32) -> Int {
+    return Int(random() % Int(max + 1))
+}
+#endif
+
 let drop = Droplet()
 
 drop.get("/") { request in
@@ -19,6 +25,11 @@ drop.get("blog") { request in
     let fullPage = header + footer + pageContent
 
     return Response(status: .ok, body: fullPage)
+}
+
+drop.get("json") { request in
+
+    return try JSON(JSONCreator().generateJSON())
 }
 
 let port = drop.config["app", "port", "host"].int ?? 80
